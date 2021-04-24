682f6d5df6f32cbf0d93b23ea3182a411249e37e
Matt Ellen <127148+Mellen@users.noreply.github.com>
2021-04-24 16:46:50 +0100

    created the database

--- add.py
+++ add.py
@@ -9,10 +9,31 @@ db_file="questions.db"
 def createDB():
     print("creating the database")
     con = sqlite3.connect(db_file)
+    cur = con.cursor()
+
+    cur.execute('''CREATE TABLE questions
+                   (qid INTEGER NOT NULL PRIMARY KEY,
+                    site TEXT NOT NULL,
+                    seqid INTEGER NOT NULL,
+                    created_date TEXT NOT NULL,
+                    title TEXT NOT NULL,
+                    active INTEGER NOT NULL,
+                    UNIQUE(site, seqid));''')
+
+    cur.execute('''CREATE TABLE views
+                   (vid INTEGER NOT NULL PRIMARY KEY,
+                    qid INTEGER NOT NULL,
+                    view_count INTEGER NOT NULL,
+                    record_date TEXT,
+                    FOREIGN KEY(qid) REFERENCES questions(qid));''')
+    con.commit()
+    con.close()
+    print('database created')
 
 def add(site, qid):
     if not isfile(db_file):
         createDB()
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Add a question to have its view count tracked hourly.")
