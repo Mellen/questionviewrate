827d04ddf3928240fa08864e5342e67c3cc6707f
Matt Ellen <127148+Mellen@users.noreply.github.com>
2021-04-24 12:28:23 +0100

    added argument handling to the add module

--- add.py
+++ add.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 
+import argparse
+
 db_file="questions.db"
+
+def add(site, qid):
+    print(site, qid)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Add a question to have its view count tracked hourly.")
+    parser.add_argument('site', help='The name of the site, e.g. english, where the question is located.')
+    parser.add_argument('id', help='The id of the question to stop tracking, e.g. 565286.')
+    args = parser.parse_args()
+
+    add(args.site, args.id)
