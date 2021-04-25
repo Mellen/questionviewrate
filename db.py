7164c11e0773f3245736d93a22bfc77df2adbfb4
Matt Ellen <127148+Mellen@users.noreply.github.com>
2021-04-25 21:58:10 +0100

    if the question is already there, set it to active

--- db.py
+++ db.py
@@ -33,21 +33,52 @@ def addQuestion(site, data):
     cur = con.cursor()
 
     question = data['items'][0]
-    
-    cur.execute('''INSERT INTO questions
+
+    try:
+        cur.execute('''INSERT INTO questions
                     (site, seqid, created_date, title, link, active)
                     VALUES
                     (?, ?, ?, ?, ?, 1);''',
-                (site, question['question_id'], question['creation_date'], question['title'], question['link']))
+                    (site, question['question_id'], question['creation_date'], question['title'], question['link']))
 
-    qid = cur.lastrowid
+        qid = cur.lastrowid
+    except sqlite3.IntegrityError:
+        cur.execute('''SELECT qid
+                       FROM questions
+                       WHERE site = :site
+                       AND seqid = :seqid
+                    ''', {'site':site, 'seqid': question['question_id']})
+
+        result = cur.fetchone()
+
+        qid = result[0]
+
+        cur.execute('''UPDATE questions
+                       SET active = 1
+                       WHERE qid = :qid''',
+                    {'qid':qid})
 
     cur.execute('''INSERT INTO views
                    (qid, view_count, record_date)
                    VALUES
                    (?, ?, ?);''',
                 (qid, question['view_count'], int(time.time())))
+
+    con.commit()
+    con.close()
+
+def addViewCount(qid, data):
+    con = sqlite3.connect(db_file)
+    cur = con.cursor()
+
+    question = data['items'][0]
     
+    cur.execute('''INSERT INTO views
+                   (qid, view_count, record_date)
+                   VALUES
+                   (?, ?, ?);''',
+                (qid, question['view_count'], int(time.time())))
+
     con.commit()
     con.close()
     
