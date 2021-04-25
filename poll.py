bfc84709893ea3e0d54e56e9368727ae4e396a1f
Matt Ellen <127148+Mellen@users.noreply.github.com>
2021-04-25 19:18:49 +0100

    implemented getting data for a question

--- poll.py
+++ poll.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
-from add import db_file
+import zlib
+import json
+from urllib.request import Request, urlopen
+
+api_base_url = 'https://api.stackexchange.com/2.2/questions/'
+
+def getQuestionData(site, qid):
+    url = f'{api_base_url}{qid}?site={site}'
+    resp = urlopen(url)
+    decompressed_bytes = zlib.decompress(resp.read(), 16+zlib.MAX_WBITS)
+    data = json.loads(decompressed_bytes.decode('utf-8'))
+    print(data)
+    return data
 
 
-        
-
+if __name__ == '__main__':
+    getQuestionData('english', 565286)
